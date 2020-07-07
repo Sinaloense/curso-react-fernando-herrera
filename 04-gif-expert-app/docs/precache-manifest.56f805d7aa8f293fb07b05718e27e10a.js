e088e3840a0734b599165fe8dce658d59f28bbbf
Manuel <sinaloensemel@gmail.com>
2020-07-06 21:24:16 -0600

    Build de produccion en carpeta docs

---
+++ 04-gif-expert-app/docs/precache-manifest.56f805d7aa8f293fb07b05718e27e10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d5eee6b329b58e06de0180899fed67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7167c6e3eccf4f7747f",
+    "url": "/static/css/main.59ae6601.chunk.css"
+  },
+  {
+    "revision": "4878dc87bf6dd747ebfe",
+    "url": "/static/js/2.6fa727a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fa727a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7167c6e3eccf4f7747f",
+    "url": "/static/js/main.28a6a277.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
