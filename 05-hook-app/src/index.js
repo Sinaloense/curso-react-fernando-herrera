f6ba889d81bfe8381dc80754fc111c449f678ea8
Sinaloense <sinaloensemel@gmail.com>
2020-07-22 11:19:11 -0600

    Seccion 9 Finalizada: Profundizando Hooks - Generales

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -4,7 +4,15 @@ import ReactDOM from 'react-dom';
 // import { CounterApp } from './componentes/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './componentes/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './componentes/02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './componentes/02-useEffect/formWithCustomHook';
+// import { FormWithCustomHook } from './componentes/02-useEffect/formWithCustomHook';
+// import { MultipleCustomHooks } from './componentes/03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './componentes/04-useRef/FocusScreen';
+// import { RealExampleRef } from './componentes/04-useRef/RealExampleRef';
+// import { Layout } from './componentes/05-useLayoutEffect/Layout';
+// import { Memorize } from './componentes/06-memos/Memorize';
+// import { MemoHook } from './componentes/06-memos/MemoHook';
+// import { CallbackHook } from './componentes/06-memos/CallbackHook';
+import { Padre } from './componentes/07-tarea-memo/Padre';
 
 import './bootstrap/css/bootstrap.css';
 
@@ -14,7 +22,15 @@ ReactDOM.render(
     {/* <CounterApp /> */}
     {/* <CounterWithCustomHook /> */}
     {/* <SimpleForm /> */}
-    <FormWithCustomHook />
+    {/* <FormWithCustomHook /> */}
+    {/* <MultipleCustomHooks /> */}
+    {/* <FocusScreen /> */}
+    {/* <RealExampleRef /> */}
+    {/* <Layout /> */}
+    {/* <Memorize /> */}
+    {/* <MemoHook /> */}
+    {/* <CallbackHook /> */}
+    <Padre />
   </React.StrictMode>,
   document.getElementById('root')
 );
